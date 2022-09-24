ea0f736d8616b92b935778e4f769dccac6a8e1f6
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-09-25 06:36:39 +0900

    map, filter

--- src/index.js
+++ src/index.js
@@ -33,47 +33,40 @@ console.log(var3);
 // const var3 = "constを宣言";
 // console.log(var3);
 
-
 // constで作った配列に上書きや追加はOK（constで定義したオブジェクトはプロパティの変更が可能）
 // 基本的にオブジェクトの定義はconstしか使わない。
 const val4 = {
   name: "やまだ",
-  age: 20,
-}
+  age: 20
+};
 console.log(val4);
 
 val4.name = "yamada";
 val4.address = "Tokyo";
 console.log(val4);
 
-
-
 // constの配列（[0]表記）
-const val5 = ['dog', 'cat'];
+const val5 = ["dog", "cat"];
 val5[0] = "bird";
 val5.push("monkey");
 console.log(val5);
 
-
 // テンプレート構文
 const namee = "さむ";
 console.log(`テンプレート構文は${namee}`);
-// 通常 
+// 通常
 // console.log("通常は" + name);
 
-
-/** 
- * 
+/**
+ *
  * アロー関数
- * 
-*/
+ *
+ */
 
 // 従来の書き方
 // function func1(str) {
 //   return str;
 // }
-　
-
 // 関数名を定数名において書ける
 const func1 = function (str) {
   return str;
@@ -81,62 +74,54 @@ const func1 = function (str) {
 
 console.log(func1("func1です"));
 
-
 // アロー関数
 const func2 = (str) => {
   return str;
-}
+};
 console.log(func2("func2です"));
-
 
 // 中の式が単一式の場合、returnを省略できる
 const func3 = (str) => str;
 
 console.log(func3("func3です"));
 
-
 const func4 = (num1, num2) => {
   return num1 + num2;
-}
+};
 
-console.log(func4(2,3));
+console.log(func4(2, 3));
 // 5　が返る
 
 const func5 = (num3, num4) => num3 - num4;
-console.log(func5(5,4));
+console.log(func5(5, 4));
 // 1 が返る
 
-
 /*
-*分割代入
-*/
+ *分割代入
+ */
 const myProfile = {
   name: "はま",
-  age: 30,
+  age: 30
 };
 
-const {  name, age } = myProfile;
-const message2 = `名前は${name}です、年齢は${age}です。`
+const { name, age } = myProfile;
+const message2 = `名前は${name}です、年齢は${age}です。`;
 console.log(message2);
 
-
-const myProfileAry = ['さむう', 34];
-const message3 =`名前は${myProfileAry[0]}です`
+const myProfileAry = ["さむう", 34];
+const message3 = `名前は${myProfileAry[0]}です`;
 console.log(message3);
 
 const [nameAry, ageAry] = myProfileAry;
 const message4 = `名前は${nameAry}です。年齢は${ageAry}歳です。`;
 console.log(message4);
 
-
 /***
  * デフォルト値、引数など
  */
 // 初期値がゲスト。
-const sayHello = (name="ゲスト") => console.log(`こんにちは、${name}`);
+const sayHello = (name = "ゲスト") => console.log(`こんにちは、${name}`);
 sayHello("さむ");
-
-
 
 /**
  * スプレッド構文
@@ -154,11 +139,26 @@ console.log(arr5);
 // 1: 20
 // 2: 30
 
+/**
+ * map, filter
+ */
+const nameArySec24 = ["田中", "やまだ", "Jakei"];
 
+const newNameArySec24 = nameArySec24.map((name) => {
+  if (name === "Jakei") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
 
+// 新しくmapで生成された配列の確認
+// ["田中さん", "やまださん", "Jakei"]
+console.log(newNameArySec24);
 
-
-
-
-
-
+const numArr24 = [1, 2, 3, 4, 5];
+const newNumArr24 = numArr24.filter((num) => {
+  return num % 2 === 0;
+});
+console.log(newNumArr24);
+// [2, 4]
